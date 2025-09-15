54c4eff7df9529151fe95036164acbe5a97dfe7a
j.w.jonkers <joris.jonkers@nedap.com>
2025-09-15 10:44:36 +0200

    Added scripts to run the different cases.

---
+++ project/nusmv/simple_case.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Navigate to /usr/src directory
+cd /usr/src || {
+  echo "Directory /usr/src does not exist. Exiting.";
+  exit 1;
+}
+
+# Check if the .smv file exists
+if [ ! -f "simple-case.smv" ]; then
+  echo "Error: File 'simple-case.smv' not found in /usr/src"
+  exit 1
+fi
+
+# Execute the NuSMV command
+echo "Running NuSMV..."
+NuSMV -pre cpp -dcx -ctt simple-case.smv
+
+# Check the exit status of the NuSMV command
+if [ $? -eq 0 ]; then
+  echo "NuSMV executed successfully."
+else
+  echo "NuSMV encountered an error."
+  exit 1
+fi
---
+++ run_trivial_case.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ensure nusmv is up (but don't attach)
+docker compose up nusmv -d
+
+# Execute the trivial case
+docker compose exec nusmv bash /usr/src/trivial_case.sh
